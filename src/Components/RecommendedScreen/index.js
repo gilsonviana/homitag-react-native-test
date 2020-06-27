dc7630e5c2eb795d9a47a9441d35cff57f0f99ad
Gilson Viana <vianawebdev@gmail.com>
2020-06-27 17:24:42 -0300

    :sparkles: Recommended screen

--- src/Components/RecommendedScreen/index.js
+++ src/Components/RecommendedScreen/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { FlatList, Animated, StyleSheet, SafeAreaView, ActivityIndicator } from 'react-native'
+import { FlatList, Animated, StyleSheet, SafeAreaView, ActivityIndicator, View, Image, Text, TouchableOpacity } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
@@ -35,63 +35,81 @@ const RecommendedScreen = ({
         extrapolate: 'clamp'
     })
 
-    useEffect(() => {
-        const tokenRequest = async () => {
-            try {
-                const token = await getToken()
-                setIsLoading(false)
-                if (token) {
-                    await getRecommendations(token)
-                }
-            } catch (e) {
-                setIsLoading(false)
-                Toast.show(
-                    'Please, check your internet connection.',
-                    {
-                        position: 70,
-                        duration: 3500,
-                        containerStyle: {
-                            backgroundColor: constants.colorDanger
-                        }
-                    }
-                )
+    const tokenRequest = async () => {
+        try {
+            const token = await getToken()
+            setIsLoading(false)
+            if (token) {
+                await getRecommendations(token)
             }
+        } catch (e) {
+            setIsLoading(false)
+            Toast.show(
+                'Please, check your internet connection.',
+                {
+                    position: 70,
+                    duration: 3500,
+                    containerStyle: {
+                        backgroundColor: constants.colorDanger
+                    }
+                }
+            )
         }
+    }
 
+    useEffect(() => {
         tokenRequest()
     }, [])
+
+    if (isLoading) {
+        return (
+            <LinearGradient style={styles.container} colors={[constants.colorPrimary, constants.colorDark, constants.colorDark]}>
+                <ActivityIndicator style={{flex: 1, alignSelf: 'center'}} color={constants.colorGray} size="large" />
+            </LinearGradient>
+        )
+    }
+
+    if (!recommendations.playlists) {
+        return (
+            <LinearGradient style={[styles.container, {justifyContent: 'center'}]} colors={[constants.colorPrimary, constants.colorDark, constants.colorDark]}>
+                <View style={{height: 300, margin: 'auto'}}>
+                    <Image blurRadius={.5} style={{flex: 1, width: undefined}} resizeMode="contain" resizeMethod="resize" source={require('../../Assets/images/void.png')} />
+                </View>
+                <Text style={{textAlign: 'center', color: constants.colorGray, marginTop: 50}}>No internet connection</Text>
+                <TouchableOpacity onPress={tokenRequest} style={{width: 200, alignSelf: 'center', marginTop: 40}}>
+                    <LinearGradient style={{borderRadius: 2, paddingVertical: 12}} colors={[constants.colorPrimary, constants.colorBlack]}>
+                        <Text style={{color: constants.colorGray, textAlign: 'center'}}>Try again</Text>
+                    </LinearGradient>
+                </TouchableOpacity>
+            </LinearGradient>
+        )
+    }
 
     return (
         <LinearGradient style={styles.container} colors={[constants.colorPrimary, constants.colorBlack, constants.colorBlack]}>
             <SafeAreaView style={{ flex: 1 }}>
-                {
-                    (isLoading) ?
-                        <ActivityIndicator style={{flex: 1, alignSelf: 'center'}} color={constants.colorGray} size="large" /> :
-                        <>
-                            <Animated.View
-                                style={{
-                                    height: headerHeight,
-                                }}
-                            >
-                                <Animated.Text style={[styles.title, { opacity: textOpacity }]}>{recommendations.message}</Animated.Text>
-                            </Animated.View>
-                            <FlatList
-                                scrollEventThrottle={16}
-                                onScroll={Animated.event(
-                                    [{
-                                        nativeEvent: { contentOffset: { y: scrollY } }
-                                    }], {
-                                    useNativeDriver: false
-                                }
-                                )}
-                                numColumns={2}
-                                style={styles.list}
-                                data={recommendations.playlists}
-                                renderItem={({ item }) => <PlaylistItem href={item.href} numOfTracks={item.tracks.total} imageUri={item.images[0].url} />}
-                                keyExtractor={(item) => item.id}
-                            />
-                        </>
-                }
+                <Animated.View
+                    style={{
+                        height: headerHeight,
+                    }}
+                >
+                    <Animated.Text style={[styles.title, { opacity: textOpacity }]}>{recommendations.message}</Animated.Text>
+                </Animated.View>
+                <FlatList
+                    scrollEventThrottle={16}
+                    onScroll={Animated.event(
+                        [{
+                            nativeEvent: { contentOffset: { y: scrollY } }
+                        }], {
+                        useNativeDriver: false
+                    }
+                    )}
+                    numColumns={2}
+                    style={styles.list}
+                    data={recommendations.playlists}
+                    renderItem={({ item }) => <PlaylistItem href={item.href} numOfTracks={item.tracks.total} imageUri={item.images[0].url} />}
+                    keyExtractor={(item) => item.id}
+                />
             </SafeAreaView>
         </LinearGradient>
     )
