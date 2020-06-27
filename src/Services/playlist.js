fa21d15d3d5b8febe550af01f35e4be5908cb709
Gilson Viana <vianawebdev@gmail.com>
2020-06-27 12:55:47 -0300

    :construction: Added get track method to playlist service

--- src/Services/playlist.js
+++ src/Services/playlist.js
@@ -5,7 +5,7 @@ export default (token) => {
     /**
      * Get a single playlist information.
      * 
-     * @param {string} url href to the playlist
+     * @param {string} url API href for the playlist
      */
     const getPlaylist = async (url) => {
         try {
@@ -23,7 +23,29 @@ export default (token) => {
         }
     }
 
+    /**
+     * Get a single track information.
+     * 
+     * @param {string} url API href for the track
+     */
+    const getTrack = async (url) => {
+        try {
+            const { data } = await axios({
+                url,
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+
+            return data
+        } catch (e) {
+            throw new Error(e)
+        }
+    }
+
     return {
-        getPlaylist
+        getPlaylist,
+        getTrack
     }
 }
