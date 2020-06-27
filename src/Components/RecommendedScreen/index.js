9f8f7e3c69c7d30b7c394acc5b33162d6acff8a5
Gilson Viana <vianawebdev@gmail.com>
2020-06-27 14:58:40 -0300

    :lipstick: Added animation to Recommended screen

--- src/Components/RecommendedScreen/index.js
+++ src/Components/RecommendedScreen/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { View, FlatList, Text, Animated, StyleSheet } from 'react-native'
+import { View, FlatList, Text, Animated, StyleSheet, SafeAreaView } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
@@ -15,9 +15,24 @@ const RecommendedScreen = ({
     getRecommendations,
     recommendations
 }) => {
-    const { useState, useEffect } = React 
-    
+    const { useState, useEffect } = React
+
     const [scrollY] = useState(new Animated.Value(0))
+
+    const HEADER_MAX_HEIGHT = 120;
+    const HEADER_MIN_HEIGHT = 0;
+
+    const headerHeight = scrollY.interpolate({
+        inputRange: [0, HEADER_MAX_HEIGHT + HEADER_MIN_HEIGHT],
+        outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+        extrapolate: 'clamp'
+    })
+
+    const textOpacity = scrollY.interpolate({
+        inputRange: [0, 50],
+        outputRange: [1, 0.25],
+        extrapolate: 'clamp'
+    })
 
     useEffect(() => {
         const tokenRequest = async () => {
@@ -28,35 +43,39 @@ const RecommendedScreen = ({
                     await getRecommendations(token)
                 }
             } catch (e) {
-                console.log("useEffect", e.response);   
+                console.log("useEffect", e.response);
             }
         }
         tokenRequest()
     }, [])
 
     return (
-        <View style={styles.container}>
-            <Animated.View>
-                <LinearGradient colors={[constants.colorPrimary, constants.colorBlack]}>
-                    <Text style={styles.title}>{recommendations.message}</Text>
-                </LinearGradient>
-            </Animated.View>
-            <FlatList 
-                scrollEventThrottle={16}
-                onScroll={Animated.event(
-                    [{
-                        nativeEvent: {contentOffset: {y: scrollY}}
-                    }], {
+        <LinearGradient style={styles.container} colors={[constants.colorPrimary, constants.colorBlack, constants.colorBlack]}>
+            <SafeAreaView style={{ flex: 1 }}>
+                <Animated.View
+                    style={{
+                        height: headerHeight,
+                    }}
+                >
+                    <Animated.Text style={[styles.title, {opacity: textOpacity}]}>{recommendations.message}</Animated.Text>
+                </Animated.View>
+                <FlatList
+                    scrollEventThrottle={16}
+                    onScroll={Animated.event(
+                        [{
+                            nativeEvent: { contentOffset: { y: scrollY } }
+                        }], {
                         useNativeDriver: false
                     }
-                )}
-                numColumns={2}
-                style={styles.list}
-                data={recommendations.playlists}
-                renderItem={({ item }) => <PlaylistItem href={item.href} numOfTracks={item.tracks.total} imageUri={item.images[0].url}/>}
-                keyExtractor={(item) => item.id}
-            />
-        </View>
+                    )}
+                    numColumns={2}
+                    style={styles.list}
+                    data={recommendations.playlists}
+                    renderItem={({ item }) => <PlaylistItem href={item.href} numOfTracks={item.tracks.total} imageUri={item.images[0].url} />}
+                    keyExtractor={(item) => item.id}
+                />
+            </SafeAreaView>
+        </LinearGradient>
     )
 }
 
@@ -66,8 +85,7 @@ const styles = StyleSheet.create({
         backgroundColor: constants.colorBlack,
     },
     title: {
-        paddingTop: 70,
-        marginBottom: 22,
+        paddingTop: 50,
         color: constants.colorGray,
         fontSize: 18,
         textAlign: 'center'
