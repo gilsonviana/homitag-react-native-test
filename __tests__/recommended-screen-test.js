6c500307f2fa719e22c70e5b4b6d0dc7bd4a1620
Gilson Viana <vianawebdev@gmail.com>
2020-06-28 23:58:34 -0300

    :white_check_mark: recommended screen

---
+++ __tests__/recommended-screen-test.js
@@ -0,0 +1,38 @@
+import 'react-native'
+import React from 'react'
+import { Provider } from 'react-redux'
+import renderer from 'react-test-renderer'
+import configureStore from 'redux-mock-store'
+import {render, cleanup} from 'react-native-testing-library';
+
+import RecommendedScreen from 'Components/RecommendedScreen'
+
+const mockStore = configureStore([])
+
+jest.useFakeTimers();
+
+afterEach(cleanup);
+
+describe('Recommended screen', () => {
+    let store;
+    let Component;
+
+    beforeEach(() => {
+        store = mockStore({
+            recommendations: {
+                message: '',
+                playlists: []
+            }
+        })
+
+        Component = renderer.create(
+            <Provider store={store}>
+                <RecommendedScreen />
+            </Provider>
+        )
+    })
+
+    it('should render with initial state', () => {
+        expect(Component.toJSON()).toMatchSnapshot()
+    })
+})
